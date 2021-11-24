3db9b81e634960f62c00fc2b03fa8f0bcccb99c6
Rémi <craco38play@gmail.com>
2021-11-24 15:30:51 +0100

    Démarage de la page d'acceuil

--- partials/_body.php
+++ partials/_body.php
@@ -1,13 +1,25 @@
 <body>
 <section>
 
-  <div>
-    <div>
-    <h2>Découvrez le nouveau court métrage Antonyme</h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dui mattis at risus, non, sed. Sed enim pharetra leo, diam pellentesque.</p>
+    <div class="accueil_banniere">
+      <div class="container">
+        <div class="accueil_texte-banniere">
+          <h1>Découvrez le nouveau court métrage Antonyme</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dui mattis at risus, non, sed. Sed enim pharetra leo, diam pellentesque.</p>      
+          <div class="accueil_voir-ba">VOIR LA BANDE ANNONCE</div>
+        </div>
+      </div>
     </div>
-   
-  </div>
+
+    <div class="accueil_video container">
+      <iframe src="https://www.youtube.com/embed/dQw4w9WgXcQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <iframe src="https://www.youtube.com/embed/dQw4w9WgXcQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <iframe src="https://www.youtube.com/embed/dQw4w9WgXcQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
+
+    <div class="accueil_bande-annonce">
+       <iframe src="https://www.youtube.com/embed/dQw4w9WgXcQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
 
   </section>
 </body>
--- partials/_header.php
+++ partials/_header.php
@@ -4,6 +4,11 @@
 <head>
     <meta charset="utf-8">
     <title>Antonyme</title>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet"> 
+    
+    <link rel="stylesheet" href="asset/style.css">
+    <link rel="stylesheet" href="asset/normalize.css">
+    <link rel="stylesheet" href="asset/accueil.css">
 </head>
 <div>
 <div class="posisition-fixe">
