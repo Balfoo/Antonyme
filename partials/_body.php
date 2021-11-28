944c2725e58a91cf40aec33dd0d36b595716ea09
Rémi <craco38play@gmail.com>
2021-11-28 19:54:21 +0100

    Page d'accueil

--- partials/_body.php
+++ partials/_body.php
@@ -1,25 +1,34 @@
 <body>
 <section>
 
+  <div class="accueil_personnage-div">
+  <div class="accueil_personnage-img"><img src="asset/img/perso1.png" alt="personnage"></div>
+
     <div class="accueil_banniere">
       <div class="container">
         <div class="accueil_texte-banniere">
           <h1>Découvrez le nouveau court métrage Antonyme</h1>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dui mattis at risus, non, sed. Sed enim pharetra leo, diam pellentesque.</p>      
-          <div class="accueil_voir-ba">VOIR LA BANDE ANNONCE</div>
+          <div class="accueil_voir-ba"><img src="asset/img/play.png" alt="play"><a id="voir-bd" href="#accueil_bande-annonce">VOIR LA BANDE ANNONCE</a></div>
         </div>
       </div>
     </div>
 
     <div class="accueil_video container">
-      <iframe src="https://www.youtube.com/embed/dQw4w9WgXcQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-      <iframe src="https://www.youtube.com/embed/dQw4w9WgXcQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-      <iframe src="https://www.youtube.com/embed/dQw4w9WgXcQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>
 
-    <div class="accueil_bande-annonce">
-       <iframe src="https://www.youtube.com/embed/dQw4w9WgXcQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+  </div>
+
+    <div class="accueil_bande-annonce" id="accueil_bande-annonce">
+       <iframe id="player_bande-annonce" width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>
+
+    <!--<script>
+      let voirbd = document.getElementById('voir-bd')
+      let playerBd = document.getElementById('player_bande-annonce')
+     voirbd.addEventListener("click", () => playerBd.playVideo() )
+    </script>-->
 
   </section>
 </body>
