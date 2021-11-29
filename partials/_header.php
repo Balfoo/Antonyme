bdd7c663ddf37574caa76b55c38b6f6262affd4e
Soltysiak Clement <soltysiak.clement@outlook.fr>
2021-11-29 11:59:54 +0100

    header et footer

--- partials/_header.php
+++ partials/_header.php
@@ -4,6 +4,11 @@
 <head>
     <meta charset="utf-8">
     <title>Antonyme</title>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet"> 
+    
+    <link rel="stylesheet" href="asset/style.css">
+    <link rel="stylesheet" href="asset/normalize.css">
+    <link rel="stylesheet" href="asset/accueil.css">
 </head>
 <div>
 <div class="posisition-fixe">
@@ -11,13 +16,13 @@
     <p>Ce site est un projet fictif dans la cadre d’un projet d’étude</p>
   </div>
   <header class="header">
-    <nav>
-    <img src="asset/img/Logo.png" alt="Antonyme">
-            <a href="index.php">Accueil</a>
+    <nav >
+      <div class="image_nav"> <img src="asset/img/Logo.png" alt="Antonyme"></div>
+  <div class="lien_nav">  <a href="index.php">Accueil</a>
             <a href="boutique.php">Boutique</a>
             <a href="membre.php">Membre</a>
-            <a href="contact.php">Contact</a>
+            <a href="contact.php">Contact</a></div>
+          
     </nav>
-
-  </header>
+</header>
 </div>
